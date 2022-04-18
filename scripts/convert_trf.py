f1ed1e0634b24cc71819d62f4b76a8ea2a5178ed
liuxinxin <cliuxinxin@gmail.com>
2022-04-18 15:53:51 +0800

    在collar中修改

--- scripts/convert_trf.py
+++ scripts/convert_trf.py
@@ -20,7 +20,7 @@ datasets = load_dataset('json', data_files= {'train': ASSETS_PATH + 'train_trf.j
 label_all_tokens = True
 # 对齐label
 def tokenize_and_align_labels(examples):
-    tokenized_inputs = tokenizer(examples["data"], truncation=True, is_split_into_words=True,padding=True,max_length=512)
+    tokenized_inputs = tokenizer(examples["data"], truncation=True, is_split_into_words=True)
 
     labels = []
     for i, label in enumerate(examples["label"]):
@@ -69,7 +69,7 @@ args = TrainingArguments(
 
 metric = load_metric("seqeval")
 
-data_collator = DataCollatorForTokenClassification(tokenizer)
+data_collator = DataCollatorForTokenClassification(tokenizer,padding=True,max_length=512)
 
 def compute_metrics(p):
     predictions, labels = p
